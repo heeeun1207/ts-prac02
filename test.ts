03a2085e676a99d1fe72decf4e4ac36bff6ff55a
jungheeeun <jungheeeun272@gmail.com>
2023-05-16 13:02:55 +0900

    sudo npm install -g ts-node

--- test.ts
+++ test.ts
@@ -1,14 +1,15 @@
-function exampleOne(object : object){
-  let value : Array<string> = [];
-  for (let key in object){
+function exampleOne(object: object) {
+  let value: Array<string> = [];
+  for (let key in object) {
     value.push(key);
   }
   return value;
 }
+
 let test = {
-  name : "heen",
-  age : "20",
-  job : "heenyjob"
-}
+  name: 'injun',
+  age: '24',
+  job: 'minam',
+};
 
 console.log(exampleOne(test));
